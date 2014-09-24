8c722b020f2c1101eb55a24261e00163156b01e4
kzmtd <kz.mitsuda@gmail.com>
2014-09-24 09:36:18 +0900

    html, script, cssのソースを追加

---
+++ script.js
@@ -0,0 +1,8 @@
+var mainCtrl = function($scope){
+    $scope.items = [
+        {"name":"Taro", "itemValue":1},
+        {"name":"Jiro", "itemValue":2},
+        {"name":"Saburo", "itemValue":3},
+        {"name":"Shiro", "itemValue":4},
+        ];
+}
