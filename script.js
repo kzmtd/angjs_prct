380c9c4c70b7fc544d36e118862bc1ddc00833d9
kzmtd <kz.mitsuda@gmail.com>
2014-09-27 23:36:22 +0900

    Controller as記法に変更

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var myApp = angular.module('myApp', []);
-myApp.controller('mainCtrl', function($scope){
-    $scope.items = [
+myApp.controller('mainCtrl', function(){
+    this.items = [
         {name:'Taro', value:1},
         {name:'Jiro', value:2},
         {name:'Saburo', value:3},
         {name:'Shiro', value:4},
         ];
-    $scope.today = new Date();
-    $scope.addUser = function(){};
+    this.today = new Date();
+    this.addUser = function(){};
 });
 
