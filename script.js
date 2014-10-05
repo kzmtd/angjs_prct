990fd6e4a0ca685fabd601d5e600b213f0b2ed00
kzmtd <kz.mitsuda@gmail.com>
2014-10-05 22:48:45 +0900

    ドットインストールのTodoアプリに変更

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('mainCtrl', function(){
-    this.items = [
-        {name:'Taro', value:1},
-        {name:'Jiro', value:2},
-        {name:'Saburo', value:3},
-        {name:'Shiro', value:4},
-        ];
-    this.today = new Date();
-    this.addUser = function(){};
+    this.tasks = [
+    	{"body":"do this 1", "done":false},
+    	{"body":"do this 2", "done":false},
+    	{"body":"do this 3", "done":true},
+    	{"body":"do this 4", "done":false},
+    ];
+    this.addNew = function(){
+    	this.tasks.push({"body":this.newTaskBody, "done":false});
+    	this.newTaskBody ='';
+    }
+    this.getDoneCount = function(){
+    	var count = 0;
+    	angular.forEach(this.tasks, function(task){
+    		count += task.done ? 1 : 0;
+    	});
+    	return count;
+    }
+    this.deleteDone = function(){
+    	var  newTasks = [];
+    	angular.forEach(this.tasks, function(task){
+    		if (!task.done) {
+    			newTasks.push(task);
+    		};
+    	});
+    	this.tasks = newTasks;
+    }
+    this.deleteTask = function(){
+    	console.log(index);
+    }
 });
 
